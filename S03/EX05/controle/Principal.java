f2b71819fc037aabe043c3165c6ca257c1fae5e7
nana <139725635+namxsj@users.noreply.github.com>
2025-09-04 19:35:46 -0300

    Create Principal.java

---
+++ S03/EX05/controle/Principal.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] vetor = new int[10];
+        System.out.println("Digite 10 números inteiros:");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            vetor[i] = scanner.nextInt();
+        }
+
+        NumeroArray ordenador = new NumeroArray(vetor);
+
+        System.out.println("\nArray original:");
+        ordenador.imprimirArray();
+
+        ordenador.bubbleSort();
+
+        System.out.println("Array ordenado (Bubble Sort):");
+        ordenador.imprimirArray();
+
+        scanner.close();
+    }
+}
