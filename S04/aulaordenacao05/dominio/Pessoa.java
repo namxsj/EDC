3b5eab3bad8b0a997a941ecdb625c3c38a31fa32
nana <139725635+namxsj@users.noreply.github.com>
2025-09-04 21:14:29 -0300

    Update Pessoa.java

--- S04/aulaordenacao05/dominio/Pessoa.java
+++ S04/aulaordenacao05/dominio/Pessoa.java
@@ -5,13 +5,13 @@ public class Pessoa {
     private int idade;
     private double altura;
 
+    // Construtor: usado para criar uma nova pessoa já com os dados preenchidos
     public Pessoa(String nome, int idade, double altura) {
         this.nome = nome;
         this.idade = idade;
         this.altura = altura;
     }
 
-    // Getters
     public String getNome() {
         return nome;
     }
@@ -24,8 +24,10 @@ public class Pessoa {
         return altura;
     }
 
+    // toString: define como a pessoa será exibida quando a gente der um "System.out.println()"
     @Override
     public String toString() {
+        // %.2f deixa a altura com 2 casas decimais
         return String.format("Nome: %s | Idade: %d | Altura: %.2f m", nome, idade, altura);
     }
 }
