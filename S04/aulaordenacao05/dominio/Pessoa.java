e38caef3f39b54400c3b78b7d1efecef4644ced5
nana <139725635+namxsj@users.noreply.github.com>
2025-09-04 21:00:10 -0300

    Create Pessoa.java

---
+++ S04/aulaordenacao05/dominio/Pessoa.java
@@ -0,0 +1,31 @@
+package dominio;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private double altura;
+
+    public Pessoa(String nome, int idade, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+
+    // Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Nome: %s | Idade: %d | Altura: %.2f m", nome, idade, altura);
+    }
+}
