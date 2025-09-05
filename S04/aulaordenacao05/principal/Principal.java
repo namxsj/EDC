0ae8fa5fe8dbef9166b800e3382bb37eb9e56578
nana <139725635+namxsj@users.noreply.github.com>
2025-09-04 21:15:51 -0300

    Update Principal.java

--- S04/aulaordenacao05/principal/Principal.java
+++ S04/aulaordenacao05/principal/Principal.java
@@ -2,8 +2,13 @@ import controle.ControlePessoas;
 
 public class Principal {
     public static void main(String[] args) {
+        // Cria um objeto do tipo ControlePessoas, que vai gerenciar o cadastro e ordenação
         ControlePessoas controle = new ControlePessoas();
+
+        // 1º passo: cadastrar as 5 pessoas (nome, idade e altura)
         controle.cadastrarPessoas();
+
+        // 2º passo: exibir o menu para o usuário escolher como quer ordenar os dados
         controle.exibirMenu();
     }
 }
